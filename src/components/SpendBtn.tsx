1689fb209e0ac6263127941ffa2fdc999409b388
RVleerobertdyer <ldyer@redventures.com>
2025-06-01 14:04:28 -0400

    feat: button work

--- src/components/SpendBtn.tsx
+++ src/components/SpendBtn.tsx
@@ -2,15 +2,14 @@ export default function SpendBtn({ onClick }: { onClick: () => void }) {
   return (
     <div className="w-[16rem] h-[16rem] bg-my-white-dark flex items-center justify-center rounded-md animate-glow">
       <div
-        onClick={() => {
+        onClickCapture={() => {
           onClick();
-          
         }}
         className="rounded-full relative
         w-[10rem] h-[10rem]  
         flex items-center justify-center bg-my-green-dark cursor-pointer"
         >
-        <span className="absolute bg-my-green-light w-[10rem] h-[10rem] rounded-full -translate-y-[8px] active:translate-y-[-2px] flex justify-center items-center text-my-black-dark text-[2rem]">SPEND</span>
+        <span className="absolute bg-my-green-light w-[10rem] h-[10rem] rounded-full -translate-y-[10px] active:translate-y-[-2px] active:bg-my-red-dark active:text-my-white-light hover:translate-y-[-6px] flex justify-center items-center text-my-black-dark text-[2rem]">SPEND</span>
       </div>
     </div>
   );
