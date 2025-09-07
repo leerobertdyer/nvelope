25c18dae5aef565965f8975dde2ee181febbeba7
RVleerobertdyer <ldyer@redventures.com>
2025-09-07 11:46:57 -0400

    cleanup

--- src/Context/DatabaseContext/DatabaseProvider.tsx
+++ src/Context/DatabaseContext/DatabaseProvider.tsx
@@ -21,7 +21,6 @@ export default function DatabaseProvider({ children }: { children: React.ReactNo
     
     useEffect(() => {
         if (user) {
-            console.log("HOWELFKJSEFO")
             loadUserData(user).then((data) => {
                 setEnvelopes(data.envelopes || []);
                 setPayDate(data.payDate);
