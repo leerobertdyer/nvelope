59fc118ad714bb1db57322954aabb848d9b23878
leerobertdyer <leerobertdyer@gmail.com>
2025-10-04 20:19:53 -0400

    feat: added Modal to ensure full screen on comonents. Noticed I have Popup already. May need to clean up

--- src/components/EditSpendingBudget.tsx
+++ src/components/EditSpendingBudget.tsx
@@ -3,52 +3,52 @@ import Button from "./Button";
 import { editTotalSpendingBudget } from "../firebase/editData";
 import { useAuth } from "../Context/AuthContext/useAuth";
 import { useDatabase } from "../Context/DatabaseContext/useDatabase";
+import Modal from "./Modal";
 
 interface IProps {
-    handleBack: () => void
+  handleBack: () => void;
 }
 
-export default function EditSpendingBudget({handleBack}: IProps) {
-    const [cashAmount, setCashAmount] = useState('');
-    const { user } = useAuth()
-    const { setTotalSpendingBudget } = useDatabase()
+export default function EditSpendingBudget({ handleBack }: IProps) {
+  const [cashAmount, setCashAmount] = useState("");
+  const { user } = useAuth();
+  const { setTotalSpendingBudget } = useDatabase();
 
-    function resetState() {
-        setCashAmount('')
-        handleBack()
-    }
+  function resetState() {
+    setCashAmount("");
+    handleBack();
+  }
 
-    async function manuallySetBudgetInDB() {
-        if (!cashAmount || !user) return;
-        await editTotalSpendingBudget(Number(cashAmount), user.uid);
-        setTotalSpendingBudget(Number(cashAmount));
-        handleBack()
-    }
+  async function manuallySetBudgetInDB() {
+    if (!cashAmount || !user) return;
+    await editTotalSpendingBudget(Number(cashAmount), user.uid);
+    setTotalSpendingBudget(Number(cashAmount));
+    handleBack();
+  }
 
-    return (
-        <div className="absolute z-9999 inset-0 bg-my-white-dark text-mywhite-dark w-full h-screen flex flex-col items-center justify-center">
-            <p className="text-lg mb-4 text-my-red-dark">Manually Adjusts Your Remaining Budget</p>
-            <input 
-                value={cashAmount}
-                onChange={(e) => setCashAmount(e.target.value)}
-                type="number" 
-                placeholder="Amount" 
-                className="max-w-[35rem] w-[80%] border-2 rounded-md p-2 bg-my-white-base text-my-green-dark mb-4 relative" />
-            
-            <div className="flex flex-col w-full gap-2 justify-center items-center">
-                <Button 
-                    onClick={manuallySetBudgetInDB}
-                    color="green"
-                >
-                    Save    
-                </Button>
-                <Button
-                    onClick={resetState}
-                    color="red"
-                >
-                    Back    
-                </Button>
-            </div>
+  return (
+    <Modal>
+      <div className="fixed z-9999 inset-0 bg-my-white-dark text-mywhite-dark w-full h-screen flex flex-col items-center justify-center">
+        <p className="text-lg mb-4 text-my-red-dark">
+          Manually Adjusts Your Remaining Budget
+        </p>
+        <input
+          value={cashAmount}
+          onChange={(e) => setCashAmount(e.target.value)}
+          type="number"
+          placeholder="Amount"
+          className="max-w-[35rem] w-[80%] border-2 rounded-md p-2 bg-my-white-base text-my-green-dark mb-4 relative"
+        />
+
+        <div className="flex flex-col w-full gap-2 justify-center items-center">
+          <Button onClick={manuallySetBudgetInDB} color="green">
+            Save
+          </Button>
+          <Button onClick={resetState} color="red">
+            Back
+          </Button>
         </div>
-        )
+      </div>
+    </Modal>
+  );
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,22 @@
+import Modal from "./Modal";
 import Nvelope from "./Nvelope";
 
-export default function Loading({text}: {text: string}) {
-    return (
-        <div className="absolute top-0 left-0 w-screen h-screen flex flex-col justify-center items-center z-9999 bg-my-white-base">
-            <Nvelope envelope={{id: "1", name: "Loading...", total: 0, spent: 0, saving: false}} kind="dash"/>
-            <p className="text-my-green-dark animate-pulse">{text}</p>
-        </div>
-    );
+export default function Loading({ text }: { text: string }) {
+  return (
+    <Modal>
+      <div className="absolute inset w-screen h-screen flex flex-col justify-center items-center z-9999 bg-my-white-base">
+        <Nvelope
+          envelope={{
+            id: "1",
+            name: "Loading...",
+            total: 0,
+            spent: 0,
+            saving: false,
+          }}
+          kind="dash"
+        />
+        <p className="text-my-green-dark animate-pulse">{text}</p>
+      </div>
+    </Modal>
+  );
 }
