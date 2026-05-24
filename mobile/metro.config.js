4cc9c3f995cc58bf4b29274986f819de6f0cde06
leerobertdyer <leerobertdyer@gmail.com>
2026-05-24 16:12:52 -0400

    Nativewind WORKING!

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("./node_modules/nativewind/dist/metro/index.js");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+
+// FORCE METRO TO TRANSPILE NATIVEWIND'S UNDERLYING ENGINE
+config.transformer.unstable_allowRequireContext = true; 
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withNativeWind(config, { input: "./global.css" });
